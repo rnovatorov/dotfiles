03d351b5df0f4c74c232fb3ea5db035212151943
Roman Novatorov <roman.novatorov@gmail.com>
2024-09-08 14:57:51 +0200

    i3: get rid of workspace 3

--- .config/i3/workspaces/3/fill.sh
+++ .config/i3/workspaces/3/fill.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-alacritty --class Alacritty,Shell --working-directory ~/code/enapter/monorepo &
-alacritty --class Alacritty,Editor --working-directory ~/code/enapter/monorepo --command env GOFLAGS="-tags=integration,lua53" nvim &
+alacritty &
